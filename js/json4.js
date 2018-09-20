9ba33d57148b213ab5263ea1b672f75797319ef2
PauloWS <pw02@hotmail.com>
2018-09-19 22:00:00 -0300

    Ok

---
+++ js/json4.js
@@ -0,0 +1,15 @@
+$(function () {
+    $.ajax({
+        url: "./json/pagina4.json",
+        success: function (resposta) {
+            $.each(resposta, function (index, itemData) {
+                $("#homeCD").append('');
+                $("#homeCD").append('<div class="col-md-4"><div id="cds" class="card mb-4 shadow-sm"><img src="' + itemData.urlimg + '">' + itemData.nome + '<br><br><div style="text-align:center;">' + itemData.preco + '</div></div>');
+            })
+        },
+        //error: caso ocorra um erro retorna a mensagem da função
+        error: function () {
+            alert('Ocorreu um erro ao carregar a página');
+        }
+    });
+});
